443333e4a5308947b3a13ebcbaee6d81dc1c5475
Evan Arsenault <ema9@queensu.ca>
2019-11-04 15:34:02 -0500

    initial commit

---
+++ app_alignment_viewer.py
@@ -0,0 +1,467 @@
+import json
+import base64
+import os
+from textwrap import dedent as d
+from datetime import datetime
+
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output
+import dash_table
+
+import flask
+import pandas as pd
+import plotly.graph_objs as go
+
+
+# running directly with Python
+if __name__ == '__main__':
+    from utils.app_standalone import run_standalone_app
+
+def remove_outlier(df_in, col_name):
+    q1 = df_in[col_name].quantile(0.25)
+    q3 = df_in[col_name].quantile(0.75)
+    iqr = q3-q1 #Interquartile range
+    fence_low  = q1-1.5*iqr
+    fence_high = q3+1.5*iqr
+    df_out = df_in.loc[(df_in[col_name] > fence_low) & (df_in[col_name] < fence_high)]
+    return df_out
+
+def preprocess(df_in): #
+    criteria = df_in['Time'].str.startswith('0')
+    df_in['Time'][criteria] = '0' + df_in['Time'][criteria]
+    df_in['Time'] = pd.to_datetime(df_in['Date'] + ' ' + df_in['Time'], format = "%m/%d/%Y %H:%M:%S")
+    df_in = df_in.drop(columns="Date")
+    return df_in
+
+#def plot_extrema():
+def trumpet_curve(df, col_time):
+    filtered_df = df[df.columns[~df.columns.isin([col_time])]]
+    print(filtered_df)
+
+    maxindex = filtered_df[filtered_df.max().idxmax()].idxmax()
+    minindex = filtered_df[filtered_df.min().idxmin()].idxmin()
+
+    print(filtered_df[filtered_df.min().idxmin()].idxmin())
+    #print(filtered_df.iloc[maxindex])
+    #print(filtered_df.iloc[minindex])
+    return maxindex,minindex
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in dataframe.columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+        ]) for i in range(min(len(dataframe), max_rows))]
+    )
+
+text_style = {
+    'color': "#506784",
+    'font-family': 'Open Sans'
+}
+
+DATAPATH = os.path.join(".", "sample_data", "alignment_viewer_")
+
+# Datasets
+with open('{}sample.fasta'.format(DATAPATH), encoding='utf-8') as data_file:
+    dataset1 = data_file.read()
+
+with open('{}p53.fasta'.format(DATAPATH), encoding='utf-8') as data_file:
+    dataset2 = data_file.read()
+
+with open('{}p53_clustalo.fasta'.format(DATAPATH), encoding='utf-8') as data_file:
+    dataset3 = data_file.read()
+
+df = pd.read_csv('https://raw.githubusercontent.com/eArsenault/permafrost/master/Sample_Data_Set.csv', skiprows=3)
+
+DATASETS = {
+    'dataset1': dataset1,
+    'dataset2': dataset2,
+    'dataset3': dataset3,
+}
+
+df = preprocess(df)
+
+col_names = [
+    'Bead #1(-0.60m)',
+    'Bead #2(-0.90m)',
+    'Bead #3(-1.20m)',
+    'Bead #4(-1.50m)',
+    'Bead #5(-1.80m)',
+    'Bead #6(-2.10m)',
+    'Bead #7(-2.40m)',
+]
+
+col_colours = [
+    'deepskyblue',
+    'dimgray',
+    'red',
+    'blue',
+    'green',
+    'yellow',
+    'brown'
+]
+
+for name in  col_names:
+    df = remove_outlier(df,name)
+
+trumpet_curve(df,'Time')
+
+def description():
+    return 'View multiple sequence alignments of genomic or protenomic sequences.'
+
+
+def header_colors():
+    return {
+        'bg_color': '#0C4142',
+        'font_color': 'white',
+    }
+
+
+def layout():
+    return html.Div(id='alignment-body', className='app-body', children=[
+        html.Div(children=[
+            html.Div(id='alignment-control-tabs', className='control-tabs', children=[
+                dcc.Tabs(
+                    id='alignment-tabs', value='what-is',
+                    children=[
+                        dcc.Tab(
+                            label='About',
+                            value='what-is',
+                            children=html.Div(className='control-tab', children=[
+                                html.H4(
+                                    className='what-is',
+                                    children='What is PermaPortal?'
+                                ),
+                                html.P(
+                                    """
+                                    The Alignment Viewer (MSA) component is used to align
+                                    multiple genomic or proteomic sequences from a FASTA or
+                                    Clustal file. Among its extensive set of features,
+                                    the multiple sequence alignment viewer can display
+                                    multiple subplots showing gap and conservation info,
+                                    alongside industry standard colorscale support and
+                                    consensus sequence. No matter what size your alignment
+                                    is, Alignment Viewer is able to display your genes or
+                                    proteins snappily thanks to the underlying WebGL
+                                    architecture powering the component. You can quickly
+                                    scroll through your long sequence with a slider or a
+                                    heatmap overview.
+                                    """
+                                ),
+                                html.P(
+                                    """
+                                    Note that the AlignmentChart only returns a chart of
+                                    the sequence, while AlignmentViewer has integrated
+                                    controls for colorscale, heatmaps, and subplots allowing
+                                    you to interactively control your sequences.
+                                    """
+                                ),
+                                html.P(
+                                    """
+                                    Read more about the component here:
+                                    https://github.com/plotly/react-alignment-viewer
+                                    """
+                                ),
+                            ])
+                        ),
+                        dcc.Tab(
+                            label='Add Data',
+                            value='alignment-tab-select',
+                            children=html.Div(className='control-tab', children=[
+                                html.Div(className='app-controls-block', children=[
+                                    html.Div(
+                                        className='fullwidth-app-controls-name',
+                                        children="Select preloaded dataset"
+                                    ),
+                                    dcc.Dropdown(
+                                        id='alignment-dropdown',
+                                        options=[
+                                            {
+                                                'label': 'Sample.csv',
+                                                'value': 'dataset1'
+                                            },
+                                            {
+                                                'label': 'P53.fasta naive',
+                                                'value': 'dataset2'
+                                            },
+                                            {
+                                                'label': 'P53.fasta aligned (ClustalW)',
+                                                'value': 'dataset3'
+                                            },
+                                        ],
+                                        value='dataset3',
+                                    )
+                                ]),
+
+
+                                html.Div(className='app-controls-block', children=[
+                                    html.Div(className='fullwidth-app-controls-name',
+                                             children="Upload your own dataset"),
+                                    html.A(
+                                        html.Button(
+                                            "Download sample data",
+                                            className='control-download'
+                                        ),
+                                        href="/assets/sample_data/p53_clustalo.fasta",
+                                        download="p53_clustalo.fasta",
+                                    ),
+                                    html.Div(id='alignment-file-upload-container', children=[
+                                        dcc.Upload(
+                                            id='alignment-file-upload',
+                                            className='control-upload',
+                                            children=html.Div([
+                                                "Drag and drop files or select files."
+                                            ]),
+                                        )
+                                    ])
+                                ]),
+
+                                dcc.RadioItems(
+                                    id='view-radio',
+                                    options=[
+                                        {'label': 'Table view', 'value': 'table'},
+                                        {'label': 'Graph view', 'value': 'graph'},
+                                    ],
+                                    value='table'
+                                )  
+                            ])
+                        )
+                    ],
+                ),
+            ]),
+        ]),
+        html.Div(id='permafrost-graph-panel', children=[ 
+            html.Div(id='permafrost-graph-grid',className='permafrost-graph-grid',children=[ #dcc.Loading(className='dashbio-loading', children=
+                html.Div(id='permafrost-main-graph',className='permafrost-main-graph',children=[
+                    dcc.Graph(
+                            id = 'graph-temptime',
+                            hoverData={"points": [{"x": datetime.strptime("2010-01-01 00:00","%Y-%m-%d %H:%M")}]}
+                    )
+                ]),
+                html.Div(id='permafrost-label-first',className='permafrost-label-first',children=[
+                    html.Label('Start Date (MM/DD/YYYY)'),
+                    dcc.Input(id = 'input-text',value='09/25/1999', type='text'),
+                ]),
+                html.Div(id='permafrost-label-second',className='permafrost-label-second',children=[
+                    html.Label('End Date (MM/DD/YYYY)'),
+                    dcc.Input(id = 'end-text',value='08/29/2010', type='text'),
+                ]),
+                html.Div(id='permafrost-sub-graph',className='permafrost-sub-graph',children=[
+                    dcc.Graph(
+                        id = 'graph-depthtemp',
+                    )
+                ]),
+            ]),
+        ]),
+        dcc.Store(id='alignment-data-store'),
+    ])
+
+
+def callbacks(_app):
+
+    # Handle file upload/selection into data store
+    @_app.callback(
+        Output('alignment-data-store', 'data'),
+        [Input('alignment-dropdown', 'value'),
+         Input('alignment-file-upload', 'contents'),
+         Input('alignment-file-upload', 'filename')]
+    )
+    def update_storage(dropdown, contents, filename):
+
+        if (contents is not None) and ('fasta' in filename):
+            content_type, content_string = contents.split(',')
+            content = base64.b64decode(content_string).decode('UTF-8')
+        else:
+            content = DATASETS[dropdown]
+
+        return content
+
+    @_app.callback(
+        Output('graph-temptime', 'figure'),
+        [Input('input-text', 'value'),
+         Input('end-text','value')]
+    )
+    def update_figure(start_str,end_str):
+
+        start_datetime = datetime.strptime(start_str,'%m/%d/%Y') #add a try  catch loop
+        end_datetime = datetime.strptime(end_str,'%m/%d/%Y')
+
+        filtered_df = df[(df['Time'] < end_datetime) & (df['Time'] > start_datetime)]
+        traces = []
+        for i in range(7): #avoid magic numbers
+            traces.append(go.Scatter(
+                x=filtered_df['Time'],
+                y=filtered_df[col_names[i]],
+                opacity=0.8,
+                line_color=col_colours[i],
+                name=col_names[i]
+            ))
+        
+        traces.append(go.Scatter(
+            x=[filtered_df['Time'].min(),filtered_df['Time'].max()],
+            y=[0,0],
+            line={'color':'black','dash':'dash'},
+            name="Freezing Point",
+            opacity=0.5,
+        ))
+
+        return {
+            'data': traces,
+            'layout': go.Layout(
+                margin={'l': 40, 'b': 40, 't': 40, 'r': 40},
+
+                legend_orientation='h',
+                hovermode='closest',
+                title="test",
+                yaxis={'zeroline':False}
+            )
+        }
+
+    @_app.callback(
+        Output('graph-depthtemp', 'figure'),
+        [Input('graph-temptime', 'hoverData')]
+    )
+    def display_depthdata(hoverData):
+        df_noTime = df[df.columns[~df.columns.isin(['Time'])]]
+        filtered_df = df_noTime.loc[df['Time'] == hoverData['points'][0]['x']]
+        
+        maxindex, minindex = trumpet_curve(df,"Time")
+        print(minindex)
+        #print(df.iloc[minindex])
+        traces = [go.Scatter(
+            x=filtered_df.squeeze(),
+            y=[-0.60,-0.90,-1.20,-1.50,-1.80,-2.10,-2.40],
+            opacity=0.8,
+            line_color=col_colours[0],
+            name = "Ground Depth"
+        ),
+        go.Scatter(
+            x=[0,0],
+            y=[-2.4,-.6],
+            line={'color':'black','dash':'dash'},
+            name="Freezing Point",
+            opacity=0.5,
+        ),
+        go.Scatter(
+            x=df_noTime.loc[maxindex],
+            y=[-0.60,-0.90,-1.20,-1.50,-1.80,-2.10,-2.40],
+            line={'color':'black','dash':'dash'},
+            name="Max",
+            opacity=0.5,
+        ),
+        go.Scatter(
+            x=df_noTime.loc[minindex],
+            y=[-0.60,-0.90,-1.20,-1.50,-1.80,-2.10,-2.40],
+            line={'color':'black','dash':'dash'},
+            name="Min",
+            opacity=0.5,
+        )]
+
+        return {
+            'data': traces,
+            'layout': go.Layout(
+                margin={'l': 40, 'b': 40, 't': 40, 'r': 40},
+                hovermode='closest',
+                xaxis={'range':[-30,15], 'zeroline': False},
+                title="test",
+
+                legend_orientation='h',
+                showlegend=True,
+            )
+        }
+
+    @_app.callback(
+        Output('permafrost-graph-panel', 'children'),
+        [Input('alignment-tabs', 'value'),
+        Input('view-radio', 'value')])
+    def render_content(tab,radio):
+        if tab == 'what-is':
+            return html.Div([dcc.Graph(id='permafrost-map',
+                    figure={
+                        'data':[go.Scattermapbox(
+                            lat = ['44.2250', '44.0'],
+                            lon = ['-76.4951', '-76.0'],
+                            
+                            mode = 'markers',
+                            marker = go.Marker(
+                                size = 11
+                            ),
+
+                            name="Sites",
+                            text=["Test Site #1","Test Site #2"],
+                            hoverinfo='text',
+                            hovertext=["Test Site #1","Test Site #2"]
+                        )],
+                        'layout': go.Layout(
+                            margin={'l': 10, 'b': 10, 't': 10, 'r': 10},
+                            autosize = True,
+                            showlegend = True,
+                            hovermode = 'closest',
+                            geo = dict(
+                                projection = dict(type = "equirectangular"),
+                                ),
+                            mapbox = dict(
+                                center = dict(
+                                    lat = 44.2250,
+                                    lon = -76.4951
+                                    ),
+                                pitch = 0,
+                                zoom = 8,
+                                style = 'open-street-map'
+                            )
+                        )
+                    }
+            )
+            ])
+                
+        elif radio == 'table':
+            return dash_table.DataTable(
+                id='table',
+                columns=[{"name": i, "id": i} for i in df.columns],
+                data=df.to_dict("rows"),
+                style_table={
+                    'maxHeight': '100%',
+                    'overflowY': 'scroll'
+                })
+                
+                #generate_table(df)
+        else:
+            return html.Div(id='permafrost-graph-grid',className='permafrost-graph-grid',children=[
+                html.Div(id='permafrost-main-graph',className='permafrost-main-graph',children=[
+                    dcc.Graph(
+                            id = 'graph-temptime',
+                            hoverData={"points": [{"x": datetime.strptime("2010-01-01 00:00","%Y-%m-%d %H:%M")}]}
+                    )
+                ]),
+                html.Div(id='permafrost-label-first',className='permafrost-label-first',children=[
+                    html.Label('Start Date (MM/DD/YYYY)'),
+                    dcc.Input(id = 'input-text',value='09/25/1999', type='text'),
+                ]),
+                html.Div(id='permafrost-label-second',className='permafrost-label-second',children=[
+                    html.Label('End Date (MM/DD/YYYY)'),
+                    dcc.Input(id = 'end-text',value='08/29/2010', type='text'),
+                ]),
+                html.Div(id='permafrost-sub-graph',className='permafrost-sub-graph',children=[
+                    dcc.Graph(
+                        id = 'graph-depthtemp',
+                    )
+                ]),
+            ])
+
+    @_app.callback(
+        Output('click-data', 'children'),
+        [Input('permafrost-map', 'clickData')])
+    def display_click_data(clickData):
+        return json.dumps(clickData, indent=2)
+        
+# only declare app/server if the file is being run directly
+if 'DASH_PATH_ROUTING' in os.environ or __name__ == '__main__':
+    app = run_standalone_app(layout, callbacks, header_colors, __file__)
+    server = app.server
+
+if __name__ == '__main__':
+    app.run_server(debug=True, port=8050)
---
+++ utils/app_wrapper.py
@@ -0,0 +1,62 @@
+import base64
+
+import dash_core_components as dcc
+import dash_html_components as html
+
+
+def app_page_layout(page_layout,
+                    app_title="Dash Bio App",
+                    app_name="",
+                    light_logo=True,
+                    standalone=False,
+                    bg_color="#506784",
+                    font_color="#F3F6FA"):
+    return html.Div(
+        id='main_page',
+        children=[
+            dcc.Location(id='url', refresh=False),
+            html.Div(
+                id='app-page-header',
+                children=[
+                    
+                    html.H2(
+                        children=[app_title]
+                    ),
+
+                    html.A(
+                        id='gh-link',
+                        children=[
+                            'View on GitHub'
+                        ],
+                        href="http://github.com/plotly/dash-bio/"
+                        "blob/master/tests/dashbio_demos/app_{}.py".format(
+                            app_name
+                        ),
+                        style={'color': 'white' if light_logo else 'black',
+                               'border': 'solid 1px white' if light_logo else 'solid 1px black'}
+                    ),
+
+                    html.Img(
+                        src='data:image/png;base64,{}'.format(
+                            base64.b64encode(
+                                open(
+                                    './assets/GitHub-Mark-{}64px.png'.format(
+                                        'Light-' if light_logo else ''
+                                    ),
+                                    'rb'
+                                ).read()
+                            ).decode()
+                        )
+                    )
+                ],
+                style={
+                    'background': bg_color,
+                    'color': font_color,
+                }
+            ),
+            html.Div(
+                id='app-page-content',
+                children=page_layout
+            )
+        ],
+    )
